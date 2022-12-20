d5ddee5d8b0b9bc98eb32a00dd758ae3d5558c73
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-12-21 02:30:46 +0600

    Get methods working fine

--- routeHandler/appointmentHandler.js
+++ routeHandler/appointmentHandler.js
@@ -8,12 +8,38 @@ const Appointment = new mongoose.model("Appointment", appointmentSchema)
 
 // get all the appointments
 router.get("/", async(req, res) => {
-    
+    try {
+         await Appointment.find({}, (err, data) => {
+            if (err) {
+                res.status(500).json({ error: "There was an server side error occured" })
+            } else {
+                res.status(200).json({
+                    success: true,
+                    result: data
+                })
+            }
+         }).clone().catch((errr) => console.log(errr))
+    }catch (error) {
+        console.log(error);
+    }
 })
 
 // get an appointment
 router.get("/:id", async(req, res) => {
-
+    try {
+        await Appointment.findOne({_id: req.params.id}, (err, data) => {
+            if (err) {
+                res.status(500).json({ error: "There was an server side error occured" })
+            } else {
+                res.status(200).json({
+                    success: true,
+                    result: data
+                })
+            }
+        }).clone().catch((errr) => console.log(errr))
+    } catch (error) {
+        console.log(error);
+    }
 })
 
 
@@ -26,7 +52,7 @@ router.post("/", async(req, res) => {
         }else{
             res.status(200).json({ message: "Appointment booked successfully"})
         }
-    })
+    }).clone().catch((errr) => console.log(errr))
 })
 
 // update an appointment
@@ -38,7 +64,7 @@ router.put("/:id", async(req, res) => {
             } else {
                 res.status(200).json({ message: "Appointment updated successfully" })
             }
-        }, )
+        }).clone().catch((errr) => console.log(errr))
         
     } catch (error) {
         console.log(error);   
