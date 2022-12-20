25e6f0248e9ca317d70ba4bb0d704f8228e31d01
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-12-20 23:47:09 +0600

    Mongoose connected with mongodb successfully

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
+const cors = require("cors")
 require("dotenv").config()
 
+const app = express();
+
+
 // middleware
+app.use(cors())
 app.use(express.json())
 
 app.get("/", (req, res) => {
     res.send("Server in running on port 5000")
 })
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.n72f5gi.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
-
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.n72f5gi.mongodb.net/?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
+.then(() => console.log("Database is connected successfully"))
+.catch((err) => console.log(err.message))
 
 app.listen(5000, () => console.log("Server is running on port 5000"))
